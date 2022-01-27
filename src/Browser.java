8692ea8a7ef870537231060ed6d2ef4de7156c65
GulshanKumark21 <96422752+GulshanKumark21@users.noreply.github.com>
2022-01-27 13:30:23 +0530

    First commit

---
+++ src/Browser.java
@@ -0,0 +1,12 @@
+
+  
+public class Browser {  
+  
+    public static void main(String[] args) {  
+      
+    // System Property for Chrome Driver   
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\GulshanKumar\\eclipse-workspace\\chromedriver.exe");
+        
+    } 
+}
+      
