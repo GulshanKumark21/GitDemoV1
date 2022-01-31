0f24880afbace5780990e80f7e02f5f9003f3295
GulshanKumark21 <96422752+GulshanKumark21@users.noreply.github.com>
2022-01-31 21:15:11 +0530

    added 3 class

---
+++ src/Browser3.java
@@ -0,0 +1,39 @@
+
+  
+public class Browser {  
+  
+    public static void main(String[] args) {  
+      
+    // System Property for Chrome Driver   
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\GulshanKumar\\eclipse-workspace\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\GulshanKumar\\eclipse-workspace\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\GulshanKumar\\eclipse-workspace\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\GulshanKumar\\eclipse-workspace\\chromedriver.exe");
+        
+               System.setProperty("webdriver.chrome.driver","C:\\Users\\GulshanKumar\\eclipse-workspace\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\GulshanKumar\\eclipse-workspace\\chromedriver.exe");
+        
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\GulshanKumar\\eclipse-workspace\\chromedriver.exe");
+     System.setProperty("webdriver.chrome.driver","C:\\Users\\GulshanKumar\\eclipse-workspace\\chromedriver.exe");
+     System.setProperty("webdriver.chrome.driver","C:\\Users\\GulshanKumar\\eclipse-workspace\\chromedriver.exe");
+  System.setProperty("webdriver.chrome.driver","C:\\Users\\GulshanKumar\\eclipse-workspace\\chromedriver.exe");
+  System.setProperty("webdriver.chrome.driver","C:\\Users\\GulshanKumar\\eclipse-workspace\\chromedriver.exe");
+System.setProperty("webdriver.chrome.driver","C:\\Users\\GulshanKumar\\eclipse-workspace\\chromedriver.exe");
+System.setProperty("webdriver.chrome.driver","C:\\Users\\GulshanKumar\\eclipse-workspace\\chromedriver.exe");
+System.setProperty("webdriver.chrome.driver","C:\\Users\\GulshanKumar\\eclipse-workspace\\chromedriver.exe");
+System.setProperty("webdriver.chrome.driver","C:\\Users\\GulshanKumar\\eclipse-workspace\\chromedriver.exe");
+System.setProperty("webdriver.chrome.driver","C:\\Users\\GulshanKumar\\eclipse-workspace\\chromedriver.exe");
+System.setProperty("webdriver.chrome.driver","C:\\Users\\GulshanKumar\\eclipse-workspace\\chromedriver.exe");
+System.setProperty("webdriver.chrome.driver","C:\\Users\\GulshanKumar\\eclipse-workspace\\chromedriver.exe");
+System.setProperty("webdriver.chrome.driver","C:\\Users\\GulshanKumar\\eclipse-workspace\\chromedriver.exe");
+System.setProperty("webdriver.chrome.driver","C:\\Users\\GulshanKumar\\eclipse-workspace\\chromedriver.exe");
+System.setProperty("webdriver.chrome.driver","C:\\Users\\GulshanKumar\\eclipse-workspace\\chromedriver.exe");
+System.setProperty("webdriver.chrome.driver","C:\\Users\\GulshanKumar\\eclipse-workspace\\chromedriver.exe");
+System.setProperty("webdriver.chrome.driver","C:\\Users\\GulshanKumar\\eclipse-workspace\\chromedriver.exe");
+System.setProperty("webdriver.chrome.driver","C:\\Users\\GulshanKumar\\eclipse-workspace\\chromedriver.exe");
+System.setProperty("webdriver.chrome.driver","C:\\Users\\GulshanKumar\\eclipse-workspace\\chromedriver.exe");
+System.setProperty("webdriver.chrome.driver","C:\\Users\\GulshanKumar\\eclipse-workspace\\chromedriver.exe");
+        
+    } 
+}
+      
